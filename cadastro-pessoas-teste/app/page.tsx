62109aeba1f6fe07aad05048f1eb3f3cd271b7aa
Gustavo Henrique Batista <89535864+GuhhDev@users.noreply.github.com>
2024-01-21 16:44:51 -0300

    Delete cadastro-pessoas-teste directory

--- cadastro-pessoas-teste/app/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import Listagem from "@/components/sections/Listagem";
-
-export default function Home() {
-
-    return (
-        <Listagem/>
-    );
-}
--- cadastro-pessoas-teste/types/Contato.model.ts
+++
@@ -1,7 +0,0 @@
-export default interface Pessoa {
-    id: number;
-    nome: string;
-    cpf: string;
-    dataNascimento: string;
-    contatos: Contato[];
-}
